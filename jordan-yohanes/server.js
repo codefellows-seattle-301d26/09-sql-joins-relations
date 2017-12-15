5760416a01b3c938cafe4b018427a03fa4ca4fea
Jordan <JordanLesaca@Gmail.Com>
2017-12-15 10:43:02 -0800

    fixed sql

--- jordan-yohanes/server.js
+++ jordan-yohanes/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
@@ -25,7 +25,7 @@ app.get('/new', (request, response) => {
 
 // REVIEW: These are routes for making API calls to enact CRUD operations on our database.
 app.get('/articles', (request, response) => {
-  client.query(`SELECT * FROM articles`)
+  client.query(`SELECT * FROM articles INNER JOIN authors ON articles.author_id=authors.author_id;`)
     .then(result => {
       response.send(result.rows);
     })
