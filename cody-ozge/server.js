46d0439c6e06ef62a173658d0eeca9bb8c1c0983
Cody <williwonka14@gmail.com>
2017-12-15 10:24:19 -0800

    finished query2

--- cody-ozge/server.js
+++ cody-ozge/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://postgres:Toasted@localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
@@ -36,7 +36,7 @@ app.get('/articles', (request, response) => {
 
 app.post('/articles', (request, response) => {
   client.query(
-    'INSERT INTO authors(author, "authorUrl")VALUES ($1, $2);',
+    'INSERT INTO authors(author, "authorUrl")VALUES ($1, $2) ON CONFLICT DO NOTHING;',
     [request.body.author, request.body.authorUrl],
     function(err) {
       if (err) console.error(err);
@@ -47,8 +47,8 @@ app.post('/articles', (request, response) => {
 
   function queryTwo() {
     client.query(
-      ``,
-      [],
+      `SELECT DISTINCT author_id FROM authors WHERE author = $1`,
+      [request.body.author],
       function(err, result) {
         if (err) console.error(err);
 
