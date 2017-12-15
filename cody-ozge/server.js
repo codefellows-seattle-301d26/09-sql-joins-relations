5599b610848f6fa8b24a0d9cefbf0cac1ce2ab32
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-15 11:29:20 -0800

    tried to aupdate

--- cody-ozge/server.js
+++ cody-ozge/server.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgres://postgres:Toasted@localhost:5432/kilovolt';
-// const conString = 'postgres://localhost:5432/kilovolt';
+//const conString = 'postgres://postgres:Toasted@localhost:5432/kilovolt';
+const conString = 'postgres://localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
